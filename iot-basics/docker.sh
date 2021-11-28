dfed5f6fcf55ea97d16b01c4dc80d64f3b9dd0ec
masterpi227 <mazinemad@gmail.com>
2021-11-28 11:04:50 +0100

    Add files via upload

--- iot-basics/docker.sh
+++ iot-basics/docker.sh
@@ -9,4 +9,4 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
