f9b7640aab4a811446e39cb9826f48a298d18746
masterpi227 <mazinemad@gmail.com>
2021-12-13 20:31:18 +0100

    Add files via upload

--- iot-analytics/loudml.sh
+++ iot-analytics/loudml.sh
@@ -1,1 +1,1 @@
-docker run -p 8077:8077 -v $PWD/loudml/lib:/var/lib/loudml:rw -v $PWD/loudml/config.yml:/etc/loudml/config.yml:ro -v $PWD/ludml/model.json:/etc/loudml/model.json:ro -d loudml/loudml
+docker run -p 8077:8077 -v $(pwd)/loudml/config.yml:/etc/loudml/config.yml:ro -d loudml/loudml
