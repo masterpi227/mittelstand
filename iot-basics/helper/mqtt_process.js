2db6e942330e37f4d19e4478180cfd6fca9a164c
masterpi227 <mazinemad@gmail.com>
2023-03-14 12:35:08 +0100

    Add files via upload

--- iot-basics/helper/mqtt_process.js
+++ iot-basics/helper/mqtt_process.js
@@ -1,7 +1,15 @@
+var new_msg = {};
+new_msg["measurement"] = msg.payload["measurement"];
+var tags = msg.topic.split("/");
+new_msg.payload = [{ "value": msg.payload["value"] }, { "machine": tags[0], "sensor": tags[2] }];
+return new_msg;
+
+/*
 var tags = msg.topic.split("/");
 var value = parseFloat(msg.payload);
-if (!isNaN(value)){
+if (!isNaN(value)) {
     var new_msg = {};
-    new_msg["payload"] = [{"value": value},{"fabrik": tags[0], "machine": tags[1], "sensor": tags[2]}];
+    new_msg["payload"] = [{ "value": value }, { "fabrik": tags[0], "machine": tags[1], "sensor": tags[2] }];
     return new_msg
 }
+*/
