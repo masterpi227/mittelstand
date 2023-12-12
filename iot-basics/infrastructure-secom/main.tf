674accbb5be460de403f538043b576c74656427e
masterpi227 <mazinemad@gmail.com>
2023-12-12 09:58:12 +0100

    Add files via upload

---
+++ iot-basics/infrastructure-secom/main.tf
@@ -0,0 +1,242 @@
+provider "google" {
+  region = "us-central1"
+}
+
+
+resource "google_compute_instance" "instance-influxdb" {
+  name                      = "instance-influxdb"
+  zone                      = "us-central1-a"
+  tags                      = ["influxdb"]
+  machine_type              = "n1-standard-2"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    auto_delete = "true"
+
+    initialize_params {
+      image = "debian-cloud/debian-10"
+      size  = "10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = <<SCRIPT
+    apt update
+    apt install -y git
+    git clone https://github.com/masterpi227/mittelstand.git
+    cd mittelstand/iot-basics
+    bash docker.sh
+    bash influxdb.sh
+    SCRIPT
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_instance" "instance-grafana" {
+  name                      = "instance-grafana"
+  zone                      = "us-central1-a"
+  tags                      = ["grafana"]
+  machine_type              = "n1-standard-1"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    auto_delete = "true"
+
+    initialize_params {
+      image = "debian-cloud/debian-10"
+      size  = "10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = <<SCRIPT
+    apt update
+    apt install -y git
+    git clone https://github.com/masterpi227/mittelstand.git
+    cd mittelstand/iot-basics
+    bash docker.sh
+    bash grafana.sh
+    SCRIPT
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_instance" "instance-nodered" {
+  name                      = "instance-nodered"
+  zone                      = "us-central1-a"
+  tags                      = ["nodered"]
+  machine_type              = "n1-standard-1"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    auto_delete = "true"
+
+    initialize_params {
+      image = "debian-cloud/debian-10"
+      size  = "10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = <<SCRIPT
+    apt update
+    apt install -y git
+    cd
+    git clone https://github.com/masterpi227/mittelstand.git
+    cd mittelstand/iot-basics
+    chmod 777 node-red/data 
+    chmod 777 node-red/data/settings.js
+    chmod 777 node-red
+    chmod 777 node-red/settings.js
+    bash docker.sh
+    bash node-red.sh
+    SCRIPT
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_instance" "instance-mosquitto" {
+  name                      = "instance-mosquitto"
+  zone                      = "us-central1-a"
+  tags                      = ["mosquitto"]
+  machine_type              = "n1-standard-1"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    auto_delete = "true"
+
+    initialize_params {
+      image = "debian-cloud/debian-10"
+      size  = "10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = <<SCRIPT
+    apt update
+    apt install -y git
+    cd
+    git clone https://github.com/masterpi227/mittelstand.git
+    cd mittelstand/iot-basics
+    bash docker.sh
+    bash mosquitto_noauth.sh
+    SCRIPT
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_instance" "instance-sensorapp" {
+  name                      = "instance-sensorapp"
+  zone                      = "us-central1-a"
+  tags                      = ["sensorapp"]
+  machine_type              = "f1-micro"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    auto_delete = "true"
+
+    initialize_params {
+      image = "debian-cloud/debian-10"
+      size  = "10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = <<SCRIPT
+    apt update
+    apt install -y git python3 python3-pip
+    git clone https://github.com/masterpi227/mittelstand.git
+    cd mittelstand/iot-basics/secom-basic-app
+    pip3 install -r requirements.txt
+    python3 main.py
+    SCRIPT
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    
+    scopes = ["cloud-platform"]
+  }
+}
+
+
+
+resource "google_compute_firewall" "default-allow-grafana" {
+  project     = var.project_id
+  name        = "default-allow-grafana"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["3000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["grafana"]
+}
+
+resource "google_compute_firewall" "default-allow-nodered" {
+  project     = var.project_id
+  name        = "default-allow-nodered"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["1880"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["nodered"]
+}
---
+++ iot-basics/secom-basic-app/main.py
@@ -0,0 +1,73 @@
+import paho.mqtt.client as mqtt
+import csv
+import queue
+import time
+import json
+
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code "+str(rc))
+
+def on_message(client, userdata, msg):
+	print(msg.topic+" "+str(msg.payload))
+
+if __name__ == "__main__":
+
+	data = queue.Queue()
+
+	item_keys = {
+		"CO(GT)": "carbon_monoxide",
+		"PT08.S1(CO)": "carbon_monoxide_tin",
+		"NMHC(GT)": "non_metanic_hydrocarbone",
+		"C6H6(GT)": "benzene",
+		"PT08.S2(NMHC)": "non_metanic_hydrocarbone_titania",
+		"NOx(GT)": "nox_stickoxide",
+		"PT08.S3(NOx)": "nox_stickoxide_tungsten",
+		"NO2(GT)": "nitrogen_dioxide",
+		"PT08.S4(NO2)": "nitrogen_dioxide_tungsten",
+		"PT08.S5(O3)": "ozone_indium",
+		"T": "temperature",
+		"RH": "relative_humidity",
+		"AH": "absolute_humidity"
+	}
+
+
+	while True:
+		try:
+			
+			client = mqtt.Client()
+			client.on_connect = on_connect
+			client.on_message = on_message
+
+			client.connect("instance-mosquitto", 1883, 1800)
+			client.loop_start()
+			break
+		except:
+			print("Error in mqtt connection")
+	try:
+		with open('uci-secom.csv','r') as csvfile:
+			reader = csv.DictReader(csvfile, delimiter=",", quotechar="'") # csv.reader(csvfile, delimiter=',')
+			for row in reader:
+				data.put(row)
+	except:
+		print("Error loading csv")
+
+	while not data.empty():
+		try:
+			dt = data.get()
+			for key in dt.keys():
+				#if key in item_keys:
+				item = {
+					"measurement": "sensor",
+					"machine": ("station"+str(item_keys[key].replace("/","").strip()) if item_keys[key].find('Pass') == -1 else item_keys[key].replace("/","").strip()),
+					"timestamp": int(time.time()*1000),
+					"value": float(dt[key].replace(',','.'))
+				}
+				client.publish(topic="prodline/state/"+item['machine'], payload=json.dumps(item))
+				#else:
+				#	pass
+					#print(key)
+				time.sleep(1)
+			data.put(dt)
+			
+		except:
+			print("Error data processing")
